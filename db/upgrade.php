e7d961654d3b408c2feaef2d35ca9d67111725aa
Luis de la Torre <ldelatorre@dia.uned.es>
2016-09-16 08:59:30 +0200

    Renaming tables names for the update of the block

---
+++ db/upgrade.php
@@ -0,0 +1,56 @@
+<?php
+
+// This file is part of the Moodle block "Remlab Manager"
+//
+// Remlab Manager is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Remlab Manager is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// The GNU General Public License is available on <http://www.gnu.org/licenses/>
+//
+// Remlab Manager has been developed by:
+//  - Luis de la Torre (1): ldelatorre@dia.uned.es
+//
+//  (1): Computer Science and Automatic Control, Spanish Open University (UNED),
+//       Madrid, Spain
+
+/**
+ * Remlab manager block caps.
+ *
+ * @package    block
+ * @subpackage remlab_manager
+ * @copyright  2015 Luis de la Torre
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Upgrade function for the remlab manager block
+ *
+ * @param string $oldversion
+ * @return true
+ */
+function xmldb_ejsapp_upgrade($oldversion)
+{
+    global $DB;
+    
+    if ($oldversion <= '2016090100') {
+        // Rename sarlab_keys database table to ejsapp_sarlab_keys
+        $dbman = $DB->get_manager();
+        $table = new xmldb_table('remlab_manager_conf');
+        $dbman->rename_table($table, 'block_remlab_manager_conf');
+        $table = new xmldb_table('remlab_manager_expsyst2pract');
+        $dbman->rename_table($table, 'block_remlab_manager_exp2prc');
+        $table = new xmldb_table('remlab_manager_sarlab_keys');
+        $dbman->rename_table($table, 'block_remlab_manager_sb_keys');
+    }
+    
+   return true;
+}
