5da7097ad97468b80220a98781a8afee1e2f697a
Luis de la Torre <ldelatorre@dia.uned.es>
2018-10-25 11:51:21 +0200

    Synchronization with Sarlab IRS is now done

--- classes/task/synchronise_sarlab_users.php
+++ classes/task/synchronise_sarlab_users.php
@@ -56,7 +56,7 @@ class synchronise_sarlab_users extends \core\task\scheduled_task {
      * @throws
      */
     public function execute() {
-        global $DB;
+        global $DB, $CFG;
 
         // Obtain the list of users in Moodle with Sarlab designer role
         $sarlabdesignerroleid = $DB->get_field('role', 'id', array('shortname' => 'sarlabdesigner'));
@@ -74,8 +74,65 @@ class synchronise_sarlab_users extends \core\task\scheduled_task {
             array_push($sarlabmanagerusersid, $record->userid);
         }
 
-        // Compare info between authorized users in Sarlab and authorized users in Moodle
-        // Communicate with Sarlab to send $sarlabdesignerusersid and $sarlabmanagerusersid
+        // Ask Sarlab IRS for $sarlabirsdesignerusersid and $sarlabirsmanagerusersid
+        $sarlabirstoken = 'demo-101-irs-token';
 
+        $curl = curl_init('http://sarlabirs.dia.uned.es/designers');
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_USERPWD, $sarlabirstoken . ':');
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept: application/xml;q=0.9, */*;q=0.8'));
+        $curl_response = curl_exec($curl);
+        curl_close($curl);
+        if ($curl_response !== false) {
+            $xml_response = simplexml_load_string($curl_response);
+            if ($xml_response) {
+                $sarlabirsdesignerusersid = [];
+                $sarlabirsmanagerusersid = [];
+                $record = new \stdClass();
+                $record->contextid    = 1;
+                $record->component    = '';
+                $record->itemid       = 0;
+                $record->timemodified = time();
+                $record->modifierid   = 0;
+                $record->sortorder    = 0;
+                foreach ($xml_response->item as $item) {
+                    $siteURL = (string) $item->siteURL;
+                    $user_id = (int) $item->user_id;
+                    if ((strpos($CFG->wwwroot, $siteURL) !== false)  && $DB->record_exists('user', ['id' => $user_id])) {
+                        // Check whether the user is designer or manager and update role assignments
+                        $level = (int) $item->level;
+                        if ($level == 1) {
+                            $sarlabirsdesignerusersid[] = $user_id;
+                            if (!$DB->record_exists('role_assignments', ['roleid' => $sarlabdesignerroleid,
+                                'userid' => $user_id])) {
+                                $record->roleid = $sarlabdesignerroleid;
+                                $record->userid = $user_id;
+                                $DB->insert_record('role_assignments', $record);
+                            }
+                        } else if ($level == 0) {
+                            $sarlabirsmanagerusersid[] = $user_id;
+                            if (!$DB->record_exists('role_assignments', ['roleid' => $sarlabmanagerroleid,
+                                'userid' => $user_id])) {
+                                $record->roleid = $sarlabmanagerroleid;
+                                $record->userid = $user_id;
+                                $DB->insert_record('role_assignments', $record);
+                            }
+                        }
+                    }
+                }
+                foreach ($sarlabdesignerusersid as $sarlabdesigneruserid) {
+                    if (!in_array($sarlabdesigneruserid, $sarlabirsdesignerusersid)) {
+                        $DB->delete_records('role_assignments', ['roleid' => $sarlabdesignerroleid,
+                            'userid' => $sarlabdesigneruserid]);
+                    }
+                }
+                foreach ($sarlabmanagerusersid as $sarlabmanageruserid) {
+                    if (!in_array($sarlabmanageruserid, $sarlabirsmanagerusersid)) {
+                        $DB->delete_records('role_assignments', ['roleid' => $sarlabmanagerroleid,
+                            'userid' => $sarlabmanageruserid]);
+                    }
+                }
+            }
+        }
     }
 }
